3b200b9f81db5eda0e88b1e849d5f2b12af7e284
Tyler Holhubner <tholhubner@gmail.com>
2023-04-27 23:19:14 -0400

    quick and dirty set for data intake from AC, just to see what we can get

---
+++ app/Http/Controllers/RawDataController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RawData;
+use Illuminate\Http\Request;
+
+class RawDataController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $rawData = RawData::all();
+        return response()->json([
+            'rawData' => $rawData
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'intake_data' => 'required|json'
+        ]);
+
+        $rawData = RawData::create($request->all());
+
+        return response()->json([
+            'message' => "success",
+            'data' => $rawData
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(RawData $rawData)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(RawData $rawData)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, RawData $rawData)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(RawData $rawData)
+    {
+        //
+    }
+}
