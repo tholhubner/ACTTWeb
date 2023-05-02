a4e5549977b616d1753c907aaca1a2966b46f2e5
Tyler Holhubner <tholhubner@gmail.com>
2023-05-01 22:56:01 -0400

    removed the isAdmin check helper as I need to just use Redux for this kind of work

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -1,18 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import ApplicationLogo from '@/Components/ApplicationLogo';
 import Dropdown from '@/Components/Dropdown';
 import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import { Link } from '@inertiajs/react';
-import { isAdmin } from "../Helpers/userHelpers";
 
 export default function Authenticated({ user, header, children }) {
-    const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
-    const [isUserAdmin, setIsUserAdmin] = useState(isAdmin(user.roles))
-
-    useEffect(() => {
-        setIsUserAdmin(isAdmin(user.roles))
-    }, [user])
+    const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false)
 
     return (
         <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
--- resources/js/Pages/Admin/Index.jsx
+++ resources/js/Pages/Admin/Index.jsx
@@ -6,7 +6,7 @@ const Index = ({ auth }) => {
         <AuthenticatedLayout
             auth={auth}
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Admin Dashboardd</h2>}
+            header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Admin Dashboard</h2>}
         >
             <Head title="Admin" />
 
